d9ecb3df966fa6d86fcc3d781565648f12499fff
kapilnagar72 <kapilnagar14820@gmail.com>
2024-04-12 00:40:20 +0530

    used  encapsulated

---
+++ BankAccount.java
@@ -0,0 +1,39 @@
+
+//How to achieve encapsulation in Java? Give an example.
+
+package encapsulated_constructor_keyword;
+
+
+public class BankAccount {
+    private String accountNumber;
+    private double balance;
+
+    public BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient funds");
+        }
+    }
+}
